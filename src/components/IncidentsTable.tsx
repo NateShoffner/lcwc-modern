c14480f9fd194a8d4e4d722b7bc71b8ed4cc86f6
Nate Shoffner <nate.shoffner@gmail.com>
2023-07-21 17:41:37 -0400

    Update IncidentsTable.tsx

--- src/components/IncidentsTable.tsx
+++ src/components/IncidentsTable.tsx
@@ -24,12 +24,9 @@ function PopulatedIncidentsTable({incidents}: IncidentTableProps) {
         <Table striped bordered hover responsive>
             <thead>
                 <tr>    
-                    <th>Number</th>
-                    <th>Date</th>
+                    <th>Date/Time</th>
                     <th>Description</th>
                     <th>Location</th>
-                    <th>Agency</th>
-                    <th>Priority</th>
                     <th>Units</th>
                 </tr>
             </thead>
@@ -41,12 +38,9 @@ function PopulatedIncidentsTable({incidents}: IncidentTableProps) {
                 <tr className={incident.category.toLowerCase()} key={incident.number} style={{cursor: 'pointer'}} onClick={() => { 
                     navigate(`/view_incident/${incidents[index].number}`, {state:{incident: incidents[index]}});
                 }}>
-                    <td>{incident.number}</td>
                     <td>{new Intl.DateTimeFormat('en-US', {hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(Date.parse(incident.date))}</td>
                     <td>{normalizeDescription(incident.description)}</td>     
                     <td>{normalizeName(incident.intersection)}<br />{normalizeName(incident.municipality)}</td>
-                    <td>{normalizeAgency(incident.agency)}</td>
-                    <td>{incident.priority ?? 'N/A'}</td>
                     <td>
                         <ul className='list-unstyled'>
                             {
