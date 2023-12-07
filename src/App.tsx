c2a3c9227c589f7e497e24d67b371d43312c71a4
Nate Shoffner <nate.shoffner@gmail.com>
2023-12-07 12:17:35 -0500

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
-import './App.css';
-import { QueryClientProvider } from 'react-query';
+import "./App.css";
+import { QueryClientProvider } from "react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from '@pages/Layout';
-import NoPage from '@pages/NoPage';
-import Incidents from '@pages/Incidents';
-import Home from '@pages/Home';
-import IncidentPage from '@pages/IncidentPage';
-import MapPage from '@pages/Map';
+import Layout from "@pages/Layout";
+import NoPage from "@pages/NoPage";
+import Incidents from "@pages/Incidents";
+import Home from "@pages/Home";
+import IncidentPage from "@pages/IncidentPage";
+import MapPage from "@pages/Map";
 import { queryClient } from "@api/query-client";
-import ApiPage from '@pages/Api';
+import ApiPage from "@pages/Api";
 
 function App() {
-
-    return (
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Home />} />
-                        <Route path="incidents" element={<Incidents />} />
-                        <Route path="incident/:incidentNumber" element={<IncidentPage />} />
-                        <Route path="map" element={<MapPage />} />
-                        <Route path="api" element={<ApiPage />} />
-                        <Route path="*" element={<NoPage />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </QueryClientProvider>
-      );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="incidents" element={<Incidents />} />
+            <Route path="incident/:incidentNumber" element={<IncidentPage />} />
+            <Route path="map" element={<MapPage />} />
+            <Route path="api" element={<ApiPage />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
